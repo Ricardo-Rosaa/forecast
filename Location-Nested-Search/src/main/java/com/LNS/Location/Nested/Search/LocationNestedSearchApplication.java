9600ba5784ac3eca7bbab8b4e9d3fa822767511d
Ricardo-Rosaa <116377299+Ricardo-Rosaa@users.noreply.github.com>
2022-11-07 11:39:01 +0000

    Add files via upload

---
+++ Location-Nested-Search/src/main/java/com/LNS/Location/Nested/Search/LocationNestedSearchApplication.java
@@ -0,0 +1,13 @@
+package com.LNS.Location.Nested.Search;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocationNestedSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocationNestedSearchApplication.class, args);
+	}
+
+}
---
+++ Location-Nested-Search/src/test/java/com/LNS/Location/Nested/Search/LocationNestedSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LNS.Location.Nested.Search;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocationNestedSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
